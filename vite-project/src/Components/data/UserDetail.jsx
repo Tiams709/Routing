6df9449e7e0ec4cdfa37f92ba39b125a47caf315
OYELADUN TIAMIYU <oyeladuntaiwo12@gmail.com>
2025-09-13 12:11:45 +0100

    Added user

--- vite-project/src/Components/data/UserDetail.jsx
+++ vite-project/src/Components/data/UserDetail.jsx
@@ -11,7 +11,8 @@ export default function UserDetail() {
       <div>
           <h1>{user.name}</h1>
           <h2>{user.username}</h2>
-          <h2>{user.email }</h2>
+      <h2>{user.email}</h2>
+      <h2>{user.name}</h2>
     </div>
   )
 }
